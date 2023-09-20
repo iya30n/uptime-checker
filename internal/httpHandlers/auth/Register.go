5180aa86ea04d17088c2a0fa7ae04254eb69e9d3
yasin <yascanary0@gmail.com>
2023-09-20 11:28:36 +0330

    Feat: send html verification email

--- internal/httpHandlers/auth/Register.go
+++ internal/httpHandlers/auth/Register.go
@@ -2,11 +2,14 @@ package auth
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
 	"uptime/internal/models/User"
 	authvalidation "uptime/internal/validations/auth"
+	"uptime/pkg/config"
 	"uptime/pkg/logger"
 	"uptime/pkg/mail"
+	"uptime/pkg/view"
 
 	"github.com/gin-gonic/gin"
 )
@@ -38,6 +41,7 @@ func Register(c *gin.Context) {
 		return
 	}
 
+	// TODO: think about using transaction here (if sending mail filed, rollback the query).
 	if err := user.Save(); err != nil {
 		logger.Error(err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "something went wrong..."})
@@ -45,11 +49,26 @@ func Register(c *gin.Context) {
 	}
 
 	// TODO: send an email to verify the account (use a queue for sending mails with retry)
-	if err := mail.Send(user.Email, "Verification Email", "salam"); err != nil {
+	if err := sendVerificationEmail(user); err != nil {
 		logger.Error(err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "something went wrong..."})
 		return
 	}
 
 	c.JSON(201, gin.H{"message": "Thanks for your registration. Please check your email and verify your account"})
+}
+
+func sendVerificationEmail(user User.User) error {
+	code := rand.Intn(99999 - 10123) + 10123
+	
+	view := view.View{
+		Path: "./views/mail/verify.html",
+		Data: map[string]string{
+			"[APP_URL]": config.Get("APP_URL"),
+			"[USER_EMAIL]": user.Email,
+			"[VERIFICATION_CODE]": fmt.Sprintf("%d", code),
+		},
+	}
+
+	return mail.Send(user.Email, "Verification Email", view.Render())
 }
--- pkg/mail/Send.go
+++ pkg/mail/Send.go
@@ -2,13 +2,20 @@ package mail
 
 import (
 	"net/smtp"
+	"os"
 	"uptime/pkg/config"
 )
 
 func Send(to string, subject string, message string) error {
+	/* htmlView, err := getView("./views/mail/verify.html")
+	if err != nil {
+		return err
+	} */
+
 	message = "From: " + config.Get("SMTP_FROM") + "\r\n" +
 		"To: " + to + "\r\n" +
 		"Subject: " + subject + "\r\n" +
+		"Content-Type: " + "text/html; charset='utf-8'" +
 		"\r\n" +
 		message + "\r\n"
 
@@ -17,4 +24,10 @@ func Send(to string, subject string, message string) error {
 	address := config.Get("SMTP_HOST") + ":" + config.Get("SMTP_PORT")
 
 	return smtp.SendMail(address, auth, config.Get("SMTP_FROM"), []string{to}, []byte(message))
+}
+
+func getView(path string) (string, error) {
+	content, err := os.ReadFile(path)
+
+	return string(content), err
 }
