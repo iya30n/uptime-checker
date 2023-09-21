d049c4b2f5e1fbf2f44144eb74c316ead5e32103
yasin <yascanary0@gmail.com>
2023-09-21 15:33:41 +0330

    login validation

--- internal/httpHandlers/auth/Login.go
+++ internal/httpHandlers/auth/Login.go
@@ -1,7 +1,21 @@
 package auth
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+	"uptime/internal/validations/auth"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Login(c *gin.Context) {
-	
+	params := auth.LoginValidation{}
+	if err := c.ShouldBind(&params); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": fmt.Sprintf("%v", err.Error())})
+		return
+	}
+
+	c.JSON(200, params)
+	// check if email/username and password are not correct, return error: "invalid username or password"
+	// else, generate a jwt and return it.
 }
