6ad6a88ce282a0626766150cc5eb81bb040a0f3c
yasin <yascanary0@gmail.com>
2023-09-23 18:22:58 +0330

    Feat: add swagger to the project

--- api/http/Serve.go
+++ api/http/Serve.go
@@ -1,15 +1,25 @@
 package http
 
 import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"time"
 	"uptime/internal/httpHandlers/auth"
 	"uptime/internal/middlewares"
-
-	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func Serve() {
 	router := gin.Default()
+
+	router.Use(cors.Default())
+	router.OPTIONS("/*any", func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Status(http.StatusOK)
+	})
+
 	router.POST("/auth/register", auth.Register)
 	router.POST("/auth/verify", auth.Verify)
 	router.POST("/auth/resend-otp", middlewares.RateLimit(time.Minute*3, 1), auth.ResendOtp)
